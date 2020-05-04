22b11612dc9407ef6ba14c4bb4a073d9fd34a3e5
abhishekalekar <abhishekalekar@gmail.com>
2020-05-04 22:22:46 +1200

    Updates

---
+++ precache-manifest.ac4349d0b26e1db973426df74b9a353b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713526f0b3f5bafebcbc0baa7b3297d2",
+    "url": "/create-react-app-portfolio/index.html"
+  },
+  {
+    "revision": "38c2ad35089213370d96",
+    "url": "/create-react-app-portfolio/static/css/main.6c868577.chunk.css"
+  },
+  {
+    "revision": "bd980b3cb19998b9e72f",
+    "url": "/create-react-app-portfolio/static/js/2.9ad1d816.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/create-react-app-portfolio/static/js/2.9ad1d816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c2ad35089213370d96",
+    "url": "/create-react-app-portfolio/static/js/main.8a7ace70.chunk.js"
+  },
+  {
+    "revision": "690648b6b849564cb756",
+    "url": "/create-react-app-portfolio/static/js/runtime-main.a289ce22.js"
+  }
+]);
