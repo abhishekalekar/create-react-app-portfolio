24df0e13ee50e54fed7767e9e522c3cc22ae9b2d
abhishekalekar <abhishekalekar@gmail.com>
2021-05-31 22:53:57 +1200

    Broken Link fixes

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,10 +5,10 @@ import { SideNavItems, SideNavLink } from 'carbon-components-react/lib/component
 import { StyledSideNav } from './styles';
 
 const items = [
-  { name: 'Me', path: '/' },
-  { name: 'Projects', path: '/projects' },
-  { name: 'Work', path: '/work' },
-  { name: 'Education', path: '/education' },
+  { name: 'Me', path: '/create-react-app-portfolio/' },
+  { name: 'Projects', path: '/create-react-app-portfolio/projects' },
+  { name: 'Work', path: '/create-react-app-portfolio/work' },
+  { name: 'Education', path: '/create-react-app-portfolio/education' },
 ];
 
 const Sidebar = () => {
